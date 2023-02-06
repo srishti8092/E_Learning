201b1bbced748640f7c1242f0953469887690e65
Srishti Mishra <srishtimishra5672@gmail.com>
2023-02-06 14:11:47 +0530

    first commit

---
+++ loginsystemtut.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+	idUsers int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	uidUsers TINYTEXT NOT NULL,
+	emailUsers TINYTEXT NOT NULL,
+	pwdUsers LONGTEXT NOT NULL
+);
+
+CREATE TABLE imgupload (
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userid int(11) NOT NULL,
+	status int(11) NOT NULL
+	);
